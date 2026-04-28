882954e76dd171f49ed9f5d2acd44d9c2549d3d6
mdallamaiqbal <bd999plus13@gmail.com>
2026-04-28 20:54:35 +0600

    add google and git hub login connection

--- src/components/homepage/news/RightSideBar.jsx
+++ src/components/homepage/news/RightSideBar.jsx
@@ -1,13 +1,27 @@
+"use client"
+import { authClient } from "@/lib/auth-client";
 import { FaGithub, FaGoogle } from "react-icons/fa";
 
 
 const RightSideBar = () => {
+    const handleGoogleSignin = async ()=>{
+    const data = await authClient.signIn.social({
+    provider: "google",
+  });
+  console.log(data , "data")
+    }
+    const handleGitHubSignin = async ()=>{
+      const data = await authClient.signIn.social({
+        provider: "github"
+    });
+  console.log(data , "data")
+    }
     return (
         <div>
             <h2 className="text-lg font-bold mb-4">Login with</h2>
             <div className="flex flex-col gap-2">
-            <button className="btn border border-blue-500 text-blue-500"><FaGoogle />Login with google</button>
-            <button className="btn"><FaGithub />Login with github</button>
+            <button onClick={handleGoogleSignin} className="btn border border-blue-500 text-blue-500"><FaGoogle />Login with google</button>
+            <button onClick={handleGitHubSignin} className="btn"><FaGithub />Login with github</button>
             </div>
         </div>
     );
